d6cfae66685054002595fee9272fcf998996d87c
Xavier Lambein <xlambein@gmail.com>
2018-08-11 17:23:55 +0200

    Basic celestial physics

--- src/main.rs
+++ src/main.rs
@@ -4,30 +4,65 @@ use ggez::graphics::{DrawMode, Point2, Vector2};
 
 const G: f32 = 1.;
 
-struct MainState {
-    sun_pos: Point2,
-    sun_vel: Vector2,
-    sun_rad: f32,
-    sun_mass: f32,
+struct CelestialBody {
+    pub pos: Point2,
+    pub vel: Vector2,
+    pub rad: f32,
+    pub mass: f32,
+}
 
-    planet_pos: Point2,
-    planet_vel: Vector2,
-    planet_rad: f32,
-    planet_mass: f32,
+impl CelestialBody {
+    fn apply_gravity(&mut self, other: &CelestialBody, seconds: f32) {
+        let r = other.pos - self.pos;
+        // Newton's formula for gravity
+        // The body's own mass isn't in here, because we want the acceleration and not the force
+        let acc = G * other.mass / r.norm_squared() * r.normalize();
+        self.vel += seconds * acc;
+    }
+
+    fn update(&mut self, seconds: f32) -> GameResult<()> {
+        self.pos += self.vel * seconds;
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::circle(
+            ctx,
+            DrawMode::Fill,
+            self.pos,
+            self.rad, 2.0)?;
+        Ok(())
+    }
+}
+
+struct MainState {
+    bodies: Vec<CelestialBody>,
 }
 
 impl MainState {
     fn new(_ctx: &mut Context) -> GameResult<MainState> {
         let s = MainState {
-            sun_pos: Point2::new(400., 300.),
-            sun_vel: Vector2::new(0., 0.),
-            sun_rad: 50.,
-            sun_mass: 10000.,
+            bodies: vec![
+                CelestialBody {
+                    pos: Point2::new(400., 300.),
+                    vel: Vector2::new(0., 0.),
+                    rad: 50.,
+                    mass: 1000000.,
+                },
+                CelestialBody {
+                    pos: Point2::new(200., 300.),
+                    vel: Vector2::new(0., 50.),
+                    rad: 20.,
+                    mass: 1.,
+                },
+                CelestialBody {
+                    pos: Point2::new(500., 300.),
+                    vel: Vector2::new(0., -100.),
+                    rad: 10.,
+                    mass: 1.,
+                }
 
-            planet_pos: Point2::new(200., 300.),
-            planet_vel: Vector2::new(0., 50.),
-            planet_rad: 20.,
-            planet_mass: 1.,
+            ],
         };
         Ok(s)
     }
@@ -40,14 +75,15 @@ impl event::EventHandler for MainState {
         while timer::check_update_time(ctx, DESIRED_FPS) {
             let seconds = 1. / (DESIRED_FPS as f32);
 
-            let r = self.sun_pos - self.planet_pos;
-            self.planet_vel += G * self.planet_mass * self.sun_mass / r.norm_squared() * r.normalize();
-
-            let r = self.planet_pos - self.sun_pos;
-            self.sun_vel += G * self.planet_mass * self.sun_mass / r.norm_squared() * r.normalize();
-
-            self.sun_pos += self.sun_vel * seconds;
-            self.planet_pos += self.planet_vel * seconds;
+            for i in 0..self.bodies.len() {
+                let (body, rest) = self.bodies.split_at_mut(i+1);
+                let body = body.last_mut().unwrap();
+                for other in rest {
+                    body.apply_gravity(other, seconds);
+                    other.apply_gravity(body, seconds);
+                }
+                body.update(seconds)?;
+            }
         }
 
         Ok(())
@@ -56,17 +92,9 @@ impl event::EventHandler for MainState {
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
         graphics::clear(ctx);
 
-        graphics::circle(
-            ctx,
-            DrawMode::Fill,
-            self.sun_pos,
-            self.sun_rad, 2.0)?;
-
-        graphics::circle(
-            ctx,
-            DrawMode::Fill,
-            self.planet_pos,
-            self.planet_rad, 2.0)?;
+        for body in &mut self.bodies {
+            body.draw(ctx)?;
+        }
 
         graphics::present(ctx);
         Ok(())
